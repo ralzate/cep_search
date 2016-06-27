37204eaa56d433b24b6e831d8172f326e1d5e458
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2016-06-27 14:29:16 -0300

    botoes de login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
     else
       user = User.create_or_update_by_oauth(auth)
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Signed in!"
+      redirect_to root_url, :notice => "Logado!"
     end
   end
 
   def destroy
     reset_session
-    redirect_to root_url, :notice => 'Signed out'
+    redirect_to root_url, :notice => 'Deslogado'
   end
 end
