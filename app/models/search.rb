d944d6274657998e0cb7cc5d03e2a2b8978771fc
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2016-06-27 16:10:05 -0300

    Search calls CEPSearchService when created

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,10 @@ class Search < ActiveRecord::Base
   belongs_to :user
 
   validates :cep, presence: true, length: { is: 8 }
+
+  before_save :api_search, on: :create
+
+  def api_search
+    self.assign_attributes(CEPSearchService.search(self.cep)) if self.valid?
+  end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe Search, type: :model do
   describe 'relationships' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'callbacks' do
+    it 'should call CEPSearchService and assign its response if valid' do
+      body = {
+          "bairro" => "Setor Institucional",
+          "cep" => "76872862",
+          "cidade" => "Ariquemes",
+          "estado" => "RO",
+          "logradouro" => "Rio Madeira ",
+          "tipoDeLogradouro" => "Rua"
+      }
+      response = instance_double("HTTParty::Response", code: 200, body: body.to_json)
+      allow(HTTParty).to receive(:get).and_return(response)
+      search = Search.create(cep: 76872862)
+      expect(search.status).to eq(200)
+      expect(search.street).to eq("Rua Rio Madeira ")
+      expect(search.city_state).to eq("Setor Institucional - Ariquemes (RO)")
+    end
+
+    it 'should not call CEPSearchService if given cep is invalid' do
+      expect(CEPSearchService).not_to receive(:search)
+      Search.create(cep: 123)
+    end
+  end
 end
