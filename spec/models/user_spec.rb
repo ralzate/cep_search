73f481c7e32d210fc5261aeea512e468c1e0ae0f
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2016-06-28 12:28:21 -0300

    Spec fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:provider) }
-    it { is_expected.to validate_presence_of(:provider) }
+    it { is_expected.to validate_presence_of(:uid) }
   end
   describe 'relationships' do
     it { is_expected.to have_many(:searches) }
