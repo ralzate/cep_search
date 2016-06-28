417f13258e1f04ad4daddfb1a04229bef2a097ee
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2016-06-28 13:45:07 -0300

    Facebook app hash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '318902575107492', '7e81af7baa483d6544e8bd86acd509f3',
-           :scope => 'email,user_birthday'
+  if Rails.env == 'production'
+    provider :facebook, '261241227574661', 'ae9b2f8f808eb28f7eeb34f6c59966af',
+             :scope => 'email,user_birthday'
+  else
+    provider :facebook, '318902575107492', '7e81af7baa483d6544e8bd86acd509f3',
+             :scope => 'email,user_birthday'
+  end
 end
