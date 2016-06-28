eb81498d16b1de09d85d0708a167cc85005e148d
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2016-06-28 12:16:49 -0300

    Request error handling

--- app/services/cep_search_service.rb
+++ app/services/cep_search_service.rb
@@ -1,16 +1,23 @@
 require 'httparty'
 class CEPSearchService
   def self.search(cep)
-    response = HTTParty.get('http://correiosapi.apphb.com/cep/'+cep)
-    body = JSON.parse response.body
-    attributes = { status: response.code }
-    if response.code == 200
-      attributes[:street] = "#{body["tipoDeLogradouro"]} #{body["logradouro"]}"
-      attributes[:city_state] = "#{body["bairro"]} - #{body["cidade"]} (#{body["estado"]})"
-    elsif response.code == 404
-      attributes[:error] = body["message"]
-    else
-      attributes[:error] = body
+    attributes = {}
+    begin
+      response = HTTParty.get('http://correiosapi.apphb.com/cep/'+cep)
+      body = JSON.parse response.body
+      attributes[:status] = response.code
+      if response.code == 200
+        attributes[:street] = "#{body["tipoDeLogradouro"]} #{body["logradouro"]}"
+        attributes[:city_state] = "#{body["bairro"]} - #{body["cidade"]} (#{body["estado"]})"
+      elsif response.code == 404
+        attributes[:error] = body["message"]
+      else
+        attributes[:error] = body
+      end
+    rescue HTTParty::Error
+      attributes[:error] = "Erro na requisição. Por favor, tente novamente."
+    rescue StandardError
+      attributes[:error] = "Serviço indisponível :("
     end
     attributes
   end
